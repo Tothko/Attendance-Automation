bf3436dc46fe4b047a8ce24b5eac5f97857528b4
RebelStoke <kulumus.kezzi@gmail.com>
2019-02-04 14:03:43 +0100

    LoginWindowUpdate

--- src/attendance/automation/gui/controller/LoginViewController.java
+++ src/attendance/automation/gui/controller/LoginViewController.java
@@ -7,6 +7,7 @@ package attendance.automation.gui.controller;
 
 import attendance.automation.bll.AAManager;
 import attendance.automation.dal.DALException;
+import com.jfoenix.controls.JFXSpinner;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +20,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -41,6 +43,8 @@ public class LoginViewController implements Initializable {
     private AAManager manager;
     private double xOffset = 0;
     private double yOffset = 0;
+    @FXML
+    private Label loginFailed;
     /**
      * Initializes the controller class.
      */
@@ -62,7 +66,6 @@ public class LoginViewController implements Initializable {
     private void loginMethod(ActionEvent event) throws DALException, IOException, InterruptedException {
         String login = loginField.getText();
         String password = passwordField.getText();
-        Thread.sleep(200);
         if(manager.checkLogin(login, password)){
             manager.setUser();
             if(!manager.isTeacher()){
@@ -74,6 +77,9 @@ public class LoginViewController implements Initializable {
         
         Stage stage2 = (Stage)loginField.getScene().getWindow();
         stage2.close();}
+        else{
+        loginFailed.setText("Login failed!");
+        }
     }
     
     private void studentMainViewInitialization() throws IOException{
